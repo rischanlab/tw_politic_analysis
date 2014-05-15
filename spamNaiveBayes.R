219dff7ba8a671b270f16ad2087db4a1351c5fc0
Rischan Mafrur <rischanlab@gmail.com>
2014-05-15 01:30:23 -0700

    try Naive Bayes on R for classify spam on library dataset

---
+++ spamNaiveBayes.R
@@ -0,0 +1,30 @@
+install.packages('ElemStatLearn')
+library('ElemStatLearn')
+library("klaR")
+library("caret")
+
+sub = sample(nrow(spam), floor(nrow(spam) * 0.9))
+sub
+head (sub)
+names(sub)
+train = spam[sub,]
+test = spam[-sub,]
+
+train
+test
+
+xTrain = train[,-58]
+yTrain = train$spam
+
+xTrain
+yTrain
+
+xTest = test[,-58]
+yTest = test$spam
+
+xTest
+yTest
+
+model = train(xTrain,yTrain,'nb',trControl=trainControl(method='cv',number=10))
+
+prop.table(table(predict(model$finalModel,xTest)$class,yTest))
